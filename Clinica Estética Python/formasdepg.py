01362a61db185747bfeaa30e9759999f788c93e3
JaquelineMachado <141187558+JaquelineMachado@users.noreply.github.com>
2023-10-02 20:22:19 -0300

    formasdepg.py

--- Clinica Estética Python/formasdepg.py
+++ Clinica Estética Python/formasdepg.py
@@ -25,41 +25,41 @@ def fpg():
     txtTitle = Label(frameLdd1,text="Formas de Pagamento", font="arial 18 bold italic", fg='purple')
     txtTitle.grid(row=0,column=0)
     
-    Receipt_Ref = StringVar() 
+    '''Receipt_Ref = StringVar() 
     Tax = StringVar()
-    Total = StringVar()
+    Total = StringVar()'''
     
     Date1 = StringVar()
     Date1.set(time.strftime("%d\%m\%Y"))
     Date1 = Label(frameLdd1,text="Data da Baixa",justify='left',padx=1,font="arial 10 bold", fg='purple')
-    Date1.grid(row=1,column=0,sticky=W,columnspan=1)
+    Date1.grid(row=1,column=0,pady=2,sticky=W)
     Date1= Entry(frameLdd1,justify='left')
-    Date1.grid(row=1,column=1,sticky=W, pady=4)
+    Date1.grid(row=1,column=1,pady=2,sticky=W)
     
      
     fdp = Label(frameLdd1, text="Formas de Pagamento",justify='left',padx=1,font="arial 10 bold", fg='purple')
-    fdp.grid(row=2,column=0,sticky=W)
+    fdp.grid(row=2,column=0,pady=2,sticky=W)
     fdep = ctk.CTkOptionMenu(frameLdd1,width=255,values=["Formas de Pagamento...","Cartão de Crédito","Cartão de Débito","Dinheiro","PIX"],fg_color="MediumPurple1",button_color="MediumPurple3",button_hover_color="MediumPurple3",dropdown_fg_color="MediumPurple1",dropdown_text_color="Purple")
-    fdep.grid(row=2,column=1)
+    fdep.grid(row=2,column=1,pady=2)
     
 
     pcl = Label(frameLdd1, text="Parcelas",justify='left',padx=1,font="arial 10 bold", fg='purple')
-    pcl.grid(row=3,column=0,sticky=W)
+    pcl.grid(row=3,column=0,pady=2,sticky=W)
     pcls = ctk.CTkOptionMenu(frameLdd1,width=255,values=["Parcelas...","À Vista","2 parcelas","3 parcelas","4 parcelas","5 parcelas","6 parcelas"],fg_color="MediumPurple1",button_color="MediumPurple3",button_hover_color="MediumPurple3",dropdown_fg_color="MediumPurple1",dropdown_text_color="Purple")
-    pcls.grid(row=3,column=1)
+    pcls.grid(row=3,column=1,pady=2)
     
 
 
-    framelde = Frame(frameLdd1,width=500,height=200,bg='MediumPurple1',bd= 10,relief=RIDGE)
+    framelde = Frame(frameLdd,width=300,height=200,bg='MediumPurple1',bd= 10,relief=RIDGE)
     framelde.grid(row=1,column=5,sticky=E)
 
     framelde1 = Frame(framelde,bg='white',relief=RIDGE)
     framelde1.grid(row=0,column=0,sticky=E)
 
-    txtReceipt  = Text(framelde1,height=27,width=30,bd=24,font="arial 9 bold")
+    txtReceipt  = Text(framelde1,height=20,width=30,bd=24,font="arial 9 bold")
     txtReceipt.grid(row=0,column=0)
 
-    framelde2 = Frame(frameLdd1,width=500,height=200,bg='MediumPurple1',bd= 10,relief=RIDGE)
+    framelde2 = Frame(frameLdd,width=300,height=200,bg='MediumPurple1',bd= 10,relief=RIDGE)
     framelde2.grid(row=2,column=5,columnspan=2,sticky=E)
 
     btnImpr = Button(framelde2,padx=1,pady=1,bd=4,font="arial 8 bold", fg='purple',width=16,text="Imprimir")
@@ -81,7 +81,5 @@ def Exit():
       return
 
     
-
-
 
     
